da2c793ff2a7e4c5c376e25f1b0612e165480847
Henry O'Brien <h.obrien17@hotmail.com>
2018-03-31 19:34:01 +1000

    Changed json type to jsonp

--- csselogic.js
+++ csselogic.js
@@ -19,7 +19,7 @@ function python() {
 	$.ajax({
 		type: "GET",
 		url: "https://python-question-gen.herokuapp.com/exp",
-		dataType: "json",
+		dataType: "jsonp",
 		success: pythonCallback
 	});
 }
